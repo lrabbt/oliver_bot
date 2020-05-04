313449fb957cfebf3de7bb597c6097c62f8320c2
Marco Bacelo <marcobacelo90@gmail.com>
2020-05-04 20:38:43 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='oliver_bot',
+   version='1.0',
+   description='A bot that answer mercado livre marketplace questions',
+   author='Marco Bacelo',
+   author_email='marcobacelo90@gmail.com',
+#   packages=['oliver_bot'],  #same as name
+   install_requires=['bar', 'greek'], #external packages as dependencies
+)
